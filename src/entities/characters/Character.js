83f308c60298981dcee9e48d48ca24a614b903d1
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2024-03-15 22:49:48 +0100

    🐛 fix speed animation

--- src/entities/characters/Character.js
+++ src/entities/characters/Character.js
@@ -2,15 +2,27 @@ export class Character {
     constructor(name, x, y, velocity) {
         this.name = name;
         this.image = new Image();
+        this.frames = new Map();
         this.position = { x, y };
         this.velocity = velocity;
+        this.animationFrame = 1;
+        this.animationTimer = 0;
+        this.animationSpeed = 16.67;
     }
 
-    update(secondsPassed, context) {
+    update(frameTime, context) {
+        this.animationTimer += frameTime * this.animationSpeed;
+
+        if (this.animationTimer >= 1) {
+            this.animationFrame++;
+            if (this.animationFrame > 6) this.animationFrame = 1;
+            this.animationTimer = 0;
+        }
+        const [, , width] = this.frames.get(`forwards-${this.animationFrame}`);
         this.position.x += this.velocity;
 
         if (
-            this.position.x > context.canvas.width - this.image.width ||
+            this.position.x > context.canvas.width - width ||
             this.position.x < 0
         ) {
             this.velocity = -this.velocity;
@@ -18,6 +30,20 @@ export class Character {
     }
 
     draw(context) {
-        context.drawImage(this.image, this.position.x, this.position.y);
+        const [x, y, width, height] = this.frames.get(
+            `forwards-${this.animationFrame}`
+        );
+
+        context.drawImage(
+            this.image,
+            x,
+            y,
+            width,
+            height,
+            this.position.x,
+            this.position.y,
+            width,
+            height
+        );
     }
 }
